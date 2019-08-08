21ad9dae8cdb3b067cc683de2fca17c7e4b7ca38
sujin08 <sujin08@naver.com>
2019-08-08 18:24:01 +0900

    0808

---
+++ mypigin.js
@@ -0,0 +1,9 @@
+$.fn.backYellow = function(){
+    this.css('background-color','yellow');
+    return this;
+}
+
+$.fn.bgColor = function(red){
+    this.css('background-color',red);
+    return this;
+}
